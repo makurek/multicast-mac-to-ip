aa94ac0d2fe9cef4849a8f0ca498ece7770000c0
Marcin Kurek <md.kurek@gmail.com>
2017-10-21 20:50:56 +0200

    idk

--- mac-to-mcast.c
+++ mac-to-mcast.c
@@ -8,6 +8,8 @@ char input_mac[] = "01:00:5e:0b:01:02";
 unsigned int b[6];
 /* multicast IP address mask - 1110 xxxx xxxx xxxx xxxx xxxx xxxx xxxx */
 unsigned int ip = 0xe0000000;
+unsigned int temp_ip;
+char out_buffer[16];
 
 /* Task 1 - get 23 LSB of input MAC address and store them in int
    I interpret each char as hex */
@@ -27,14 +29,14 @@ I have to get all combination of X, so I need to iterate 31 times
 
 for (int i = 0; i < 32; i++)
 {
-  unsigned int temp = ip;
-  temp |= (i << 23);
+  temp_ip = ip;
+  temp_ip |= (i << 23);
 
-  unsigned int o4 = (temp & 0xff000000) >> 24;
-  unsigned int o3 = (temp & 0x00ff0000) >> 16;
-  unsigned int o2 = (temp & 0x0000ff00) >> 8;
-  unsigned int o1 = temp & 0x000000ff;
-  char out_buffer[50];
+  unsigned int o4 = (temp_ip & 0xff000000) >> 24;
+  unsigned int o3 = (temp_ip & 0x00ff0000) >> 16;
+  unsigned int o2 = (temp_ip & 0x0000ff00) >> 8;
+  unsigned int o1 = temp_ip & 0x000000ff;
+
   sprintf(out_buffer, "#%u\t%u.%u.%u.%u", i, o4, o3, o2, o1 );
   printf("%s\n", out_buffer);
 }
